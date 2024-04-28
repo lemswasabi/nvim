57962250dd9ab37b6f1b67f2e495bc0f14a8f275
lemswasabi <leminhnguyen@outlook.com>
2024-04-28 15:26:59 +0200

    add additional keymaps

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>", { desc = "exit insert mode from home row"})
+vim.keymap.set("n", "<C-s>", "<cmd>update<cr>", { desc = "save file in normal mode when updated"})
+vim.keymap.set("i", "<C-s>", "<C-o><cmd>update<cr>", { desc = "save file in insert mode when updated"})
+
 vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<Cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<localleader>cr", "<cmd>!cargo run<Cr>", { desc = "cargo run"})
--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.opt.conceallevel = 2
 vim.opt.cursorline = true
