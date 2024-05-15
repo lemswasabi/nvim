0639a27d6d5897aefc7d441af2199cd256b2e907
lemswasabi <leminhnguyen@outlook.com>
2024-05-15 13:50:13 +0200

    add osc52 to copy text to clipboard through SSH

--- lua/configs/plugins/editor.lua
+++ lua/configs/plugins/editor.lua
@@ -88,4 +88,16 @@ return {
     "RRethy/vim-illuminate",
     event = "BufEnter",
   },
+
+  {
+    "ojroques/nvim-osc52",
+    config = function()
+      local osc52 = require("osc52")
+      osc52.setup({})
+
+      vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, {expr = true})
+      vim.keymap.set("n", "<leader>cc", "<leader>c_", {remap = true})
+      vim.keymap.set("v", "<leader>c", require("osc52").copy_visual)
+    end
+  },
 }
