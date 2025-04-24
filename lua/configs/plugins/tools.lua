bcee40c41bd91ec49975445348cec4765c592d6d
Leminh Nguyen <leminhnguyen@outlook.com>
2025-04-24 23:52:13 +0200

    comment out obsidian

--- lua/configs/plugins/tools.lua
+++ lua/configs/plugins/tools.lua
@@ -38,48 +38,48 @@ return {
     opts = {}
   },
 
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    cmd = { "ObsidianWorkspace" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "epwalsh/pomo.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "symbiotics_docs",
-          path = "~/vaults/work/symbiotics/docs",
-          strict = true,
-        },
-        {
-          name = "team_meeting_notes",
-          path = "~/vaults/work/symbiotics/notes/team_meeting_notes",
-          strict = true,
-        }
-      },
-
-      templates = {
-        folder = "templates",
-        date_format = "%d-%m-%Y",
-        time_format = "%H:%M",
-      },
-
-      -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-      completion = {
-        -- Set to false to disable completion.
-        nvim_cmp = true,
-        -- Trigger completion at 2 chars.
-        min_chars = 2,
-      },
-    },
-  },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",  -- recommended, use latest release instead of latest commit
+  --   lazy = true,
+  --   ft = "markdown",
+  --   cmd = { "ObsidianWorkspace" },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "epwalsh/pomo.nvim",
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = "symbiotics_docs",
+  --         path = "~/vaults/work/symbiotics/docs",
+  --         strict = true,
+  --       },
+  --       {
+  --         name = "team_meeting_notes",
+  --         path = "~/vaults/work/symbiotics/notes/team_meeting_notes",
+  --         strict = true,
+  --       }
+  --     },
+  --
+  --     templates = {
+  --       folder = "templates",
+  --       date_format = "%d-%m-%Y",
+  --       time_format = "%H:%M",
+  --     },
+  --
+  --     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  --     completion = {
+  --       -- Set to false to disable completion.
+  --       nvim_cmp = true,
+  --       -- Trigger completion at 2 chars.
+  --       min_chars = 2,
+  --     },
+  --   },
+  -- },
 
   {
     "wintermute-cell/gitignore.nvim",
