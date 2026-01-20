758bbbc181c3e36504b9e29c84f06066d74c0e1a
Leminh Nguyen <leminh.nguyen@kyomei.ai>
2026-01-20 15:38:59 +0100

    chore(options): set tabstop to 4

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -6,6 +6,7 @@ vim.opt.cursorline = true
 vim.opt.mouse = "a"
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
+vim.opt.tabstop = 4
 vim.opt.spelllang = { "en" }
 vim.opt.termguicolors = true
 
